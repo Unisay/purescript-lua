710c271648a5fbbe27bc9f945036690aedeec910
Yura Lazarev <Unisay@users.noreply.github.com>
2023-07-15 16:10:44 +0200

    Fix name shadowing

--- lib/Language/PureScript/Backend/IR/Query.hs
+++ lib/Language/PureScript/Backend/IR/Query.hs
@@ -1,6 +1,8 @@
 module Language.PureScript.Backend.IR.Query where
 
+import Control.Monad.Trans.Accum (Accum, add, execAccum)
 import Data.Map qualified as Map
+import Data.Set qualified as Set
 import Language.PureScript.Backend.IR.Linker (UberModule (..))
 import Language.PureScript.Backend.IR.Types
   ( Exp
@@ -8,8 +10,11 @@ import Language.PureScript.Backend.IR.Types
   , Qualified (..)
   , countFreeRef
   , countFreeRefs
+  , groupingNames
   , listGrouping
+  , traverseExpBottomUp
   )
+import Language.PureScript.Backend.IR.Types qualified as IR
 import Language.PureScript.Names (runModuleName)
 
 usesRuntimeLazy ∷ UberModule → Bool
@@ -37,3 +42,18 @@ findPrimModuleInExpr expr =
   Map.keys (countFreeRefs expr) & any \case
     Local _name → False
     Imported moduleName _name → runModuleName moduleName == "Prim"
+
+collectBoundNames ∷ Exp → Set Name
+collectBoundNames =
+  (`execAccum` Set.empty) . traverseExpBottomUp @_ @(Accum (Set Name)) \e →
+    case e of
+      IR.Abs (IR.unAnn → IR.ParamNamed name) _body →
+        e <$ add (Set.singleton name)
+      IR.Let groupings _body →
+        e <$ add do
+          Set.fromList
+            [ IR.unAnn iname
+            | grouping ← toList groupings
+            , iname ← groupingNames grouping
+            ]
+      _ → pure e
