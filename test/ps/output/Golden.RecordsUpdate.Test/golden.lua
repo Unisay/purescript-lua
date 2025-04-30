bd7f060219b9397162bfe2cd11d4714716196db1
Yura Lazaryev <unisay@gmail.com>
2025-04-30 10:28:22 +0200

    update golden file

--- test/ps/output/Golden.RecordsUpdate.Test/golden.lua
+++ test/ps/output/Golden.RecordsUpdate.Test/golden.lua
@@ -1,7 +1,7 @@
 local function PSLUA_object_update(o, patches)
   local o_copy = {}
   for k, v in pairs(o) do
-    local patch_v = patches
+    local patch_v = patches[k]
     if patch_v ~= nil then
       o_copy[k] = patch_v
     else
