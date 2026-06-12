47b3316bf3e82a5758485d7953b0b304dbd56623
Yura Lazarev <unisay@gmail.com>
2026-06-12 10:43:30 +0200

    test: reproduce issue #37 (undefined variable in compiled code)
    Golden test from PR #38 with the explicit export list from PR #39:
    with 'module ... (baz) where' the non-exported bindings get inlined
    and the generated Lua references an undefined variable Bind11.
    The luacheck golden suite fails with W113 until the bug is fixed.

---
+++ test/ps/golden/Golden/Issue37/Test.purs
@@ -0,0 +1,20 @@
+module Golden.Issue37.Test (baz) where
+
+import Prelude
+import Effect (Effect)
+
+baz :: Effect Unit
+baz = bar (pure unit)
+
+bar :: forall f. Monad f => f Unit -> f Unit
+bar f = do
+  f
+  _ <- pure [ foo f ]
+  pure unit
+
+foo :: forall f. Monad f => f Unit -> f Unit
+foo fn1 = do
+  _ <- fn1
+  fn1
+  fn1
+  fn1
